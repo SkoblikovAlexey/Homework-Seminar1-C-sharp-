b46493b6cc82abe95ed4d10672380afdae933129
Alexey Skoblikov <happycow32@yandex.ru>
2022-08-07 17:03:48 +0300

    Task 8 added

---
+++ task8(1 to N)/Program.cs
@@ -0,0 +1,21 @@
+﻿bool check = true;
+while (check)
+{
+    Console.WriteLine("Введите число");
+    int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i < N; i++) 
+{
+ int a = i % 2;
+    if (a == 0)
+    {
+        Console.Write(i + ", ");
+    }
+}
+    Console.WriteLine("Введите 'end' для завершения, для продолжения - любой символ");
+    string c = Console.ReadLine();
+    if (c == "end")
+    {
+        check = false;
+    }
+}
