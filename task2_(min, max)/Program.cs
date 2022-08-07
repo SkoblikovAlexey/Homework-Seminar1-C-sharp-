792c75af172d164a85de8bbfe4b75364adf0c11e
Alexey Skoblikov <happycow32@yandex.ru>
2022-08-07 15:23:28 +0300

    task2

---
+++ task2_(min, max)/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите первое и второе числа");
+float a = Convert.ToInt32(Console.ReadLine());
+float b = Convert.ToInt32(Console.ReadLine());
+float min = a;
+float max = a;
+if (a > b)
+{
+    min = b;
+    max = a;
+    Console.Write("Наименьшее число: ");
+    Console.WriteLine(min);
+    Console.Write("Наибольшее число: ");
+    Console.WriteLine(max);
+}
+else if (a < b)
+{
+    min = a;
+    max = b;
+    Console.Write("Наименьшее число: ");
+    Console.WriteLine(min);
+    Console.Write("Наибольшее число: ");
+    Console.WriteLine(max);
+}
+
+else
+{
+    Console.WriteLine("Числа равны");
+}
