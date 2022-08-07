e4add653890103361ee2a21fcd0be7c76d8b7684
Alexey Skoblikov <happycow32@yandex.ru>
2022-08-07 16:32:26 +0300

    Task 6 Even added

---
+++ task6(even)/Program.cs
@@ -0,0 +1,18 @@
+﻿bool check = true;
+while (check) {
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int b = a % 2;
+if (b == 0) {
+    Console.WriteLine("Число " + a + " чётное");
+}
+else {
+    Console.WriteLine("Число " + a + " нечётное");
+}
+Console.WriteLine("Введите 'end' для выхода, для продолжения - любой символ");
+string c = Console.ReadLine();
+if (c == "end"){
+    check = false;
+}
+}
