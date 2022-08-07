0a1e88da4bd8f0cdac7094f50c0811a97b086908
Alexey Skoblikov <happycow32@yandex.ru>
2022-08-07 15:59:42 +0300

    Rename folder

---
+++ task4(max)/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите три числа");
+float a = Convert.ToInt32(Console.ReadLine());
+float b = Convert.ToInt32(Console.ReadLine());
+float c = Convert.ToInt32(Console.ReadLine());
+float max = a;
+if (a > max)
+{
+    max = a;
+}
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+Console.Write("Максимальное число: ");
+Console.WriteLine(max);
